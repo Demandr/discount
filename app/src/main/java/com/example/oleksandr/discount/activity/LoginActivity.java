7ddb0e80fdac55ab458a87f238ae9ffd7c72e02e
Demandr <ol.demandr13108@gmail.com>
2018-03-29 18:12:36 +0300

    fix splash screen

--- app/src/main/java/com/example/oleksandr/discount/activity/LoginActivity.java
+++ app/src/main/java/com/example/oleksandr/discount/activity/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme_Launcher);
+        setTheme(R.style.AppThemeNoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
